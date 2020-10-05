b160933773257089c943c2835ea2a6f82162e0d4
morphIsmail <ismail_2016@mail.ru>
2020-10-05 19:01:56 +0300

    version 2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,133 @@
+//Поле в котором все выводится
+let input = document.querySelector('.input');
+//Сохраненная часть выражения для возведения в степень
+let power = "";
+//Вставить символ
 function insert(num) {
-    document.form.textview.value = document.form.textview.value + num;
+    if (input.textContent == 0) {
+        input.textContent = "";
+        input.textContent += num;
+    } else
+        input.textContent += num;
 }
+//Очистить все поле
 function clean() {
-    document.form.textview.value = "";
+    input.textContent = "0";
+    power = "";
 }
+//Удалить символ
 function back() {
-    var exp = document.form.textview.value;
-    document.form.textview.value = exp.substring(0, exp.length - 1);
+    let exp = input.textContent;
+    input.textContent = exp.substring(0, exp.length - 1);
+    if (input.textContent == 0) {
+        input.textContent = "0";
+    }
 }
+//Посчитать выражение
 function equal() {
-    var exp = document.form.textview.value;
+    let exp = input.textContent;
+    if (input.textContent.includes('^')) {
+        let tmp = input.textContent.split('^')
+        let num = eval(power);
+        let pow = +tmp[1]
+        input.textContent = Math.pow(num, pow);
+        power = "";
+        return;
+    }
     if (exp) {
-        document.form.textview.value = eval(exp);
+        input.textContent = eval(exp);
+    }
+}
+//Вычислить проценты
+function percent() {
+    input.textContent = eval(input.textContent) / 100;
+}
+//Для добавления констант
+function constant(name) {
+    if (input.textContent == 0) {
+        input.textContent = "";
+    }
+    if (name == "pi")
+        input.textContent += Math.PI.toFixed(8);
+    if (name == "e")
+        input.textContent += Math.E.toFixed(8);
+}
+//Корень квадратный, в квадрат в -1 степень
+function operation(name) {
+    if (name == "sqrt")
+        input.textContent = Math.sqrt(eval(input.textContent));
+    if (name == "sqr")
+        input.textContent = Math.pow(eval(input.textContent), 2);
+    if (name == "^-1")
+        input.textContent = Math.pow(eval(input.textContent), -1);
+    if (name == "^") {
+        power = input.textContent;
+        input.textContent += "^";
+    }
+}
+//Факториал числа
+function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+}
+function fact() {
+    input.textContent = factorial(+eval(input.textContent));
+}
+//Логарифмы
+function log(name) {
+    if (name == 'lg') {
+        input.textContent = Math.log10(eval(input.textContent)).toFixed(8);
+    }
+    if (name == 'ln') {
+        input.textContent = Math.log(eval(input.textContent)).toFixed(8);
+    }
+}
+//Переключение с градусов на радианы
+document.querySelector('.type').addEventListener('click', function() {
+    if (document.querySelector('.type').textContent == "deg") {
+        this.textContent = "rad";
+        console.log('Градусы')
+    } else if (document.querySelector('.type').textContent == "rad") {
+        this.textContent = "deg";
+        console.log('Радианы')
+    }
+})
+
+//Синусы и косинусы
+function f(name) {
+    if (name == 'sin') {
+        if(document.querySelector('.type').textContent == "deg") {
+            //В градусах
+            input.textContent = parseFloat(Math.sin(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
+        } else {
+            //В радианах
+            input.textContent = parseFloat(Math.sin(eval(input.textContent)).toFixed(8).toString());
+        }        
+    }
+    if (name == 'cos') {
+        if(document.querySelector('.type').textContent == "deg") {
+            //В градусах
+            input.textContent = parseFloat(Math.cos(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
+        } else {
+            //В радианах
+            input.textContent = parseFloat(Math.cos(eval(input.textContent)).toFixed(8).toString());
+        } 
+    }
+    if (name == 'tan') {
+        if(document.querySelector('.type').textContent == "deg") {
+            //В градусах
+            input.textContent = parseFloat(Math.tan(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
+        } else {
+            //В радианах
+            input.textContent = parseFloat(Math.tan(eval(input.textContent)).toFixed(8).toString());
+        }  
+    }
+    if (name == 'ctg') {
+        if(document.querySelector('.type').textContent == "deg") {
+            //В градусах
+            input.textContent = parseFloat(1/Math.tan(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
+        } else {
+            //В радианах
+            input.textContent = parseFloat(1/Math.tan(eval(input.textContent)).toFixed(8).toString());
+        } 
     }
 }
